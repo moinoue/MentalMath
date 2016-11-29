2bf024641ec4b37cfdad35dd7bf7026086390735
vidhyal <vvl.1990@gmail.com>
2016-11-28 23:44:06 -0800

    fixed issue for cos in cosine

--- MentalMath/app/src/main/java/moinoue/mentalmath/Lexer.java
+++ MentalMath/app/src/main/java/moinoue/mentalmath/Lexer.java
@@ -150,7 +150,7 @@ public class Lexer implements TokenType {
                 continue;
             }
 
-            if ( strArray[i].equals( "cosine") || strArray[i].equals("cosign")|| strArray[i].equals("arccosin")){
+            if ( strArray[i].equals( "cosine") || strArray[i].equals("cosign")|| strArray[i].equals("cosin") || strArray[i].equals("cos") || strArray[i].equals("cost")){
                 Token tok = new Token(COS, 0);
                 tokens.add(tok);
                 continue;
